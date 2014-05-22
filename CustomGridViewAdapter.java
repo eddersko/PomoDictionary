5500ffe97444f38236479e9da78d7ae194961400
Edwin Ko <eddersko@bu.edu>
2014-05-22 17:01:51 -0400

    Create CustomGridViewAdapter.java

---
+++ CustomGridViewAdapter.java
@@ -0,0 +1,67 @@
+package com.example.pomodictionary;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * 
+ * @author manish.s
+ *
+ */
+public class CustomGridViewAdapter extends ArrayAdapter<Item> {
+	Context context;
+	int layoutResourceId;
+	ArrayList<Item> data = new ArrayList<Item>();
+
+	public CustomGridViewAdapter(Context context, int layoutResourceId,
+			ArrayList<Item> data) {
+		super(context, layoutResourceId, data);
+		this.layoutResourceId = layoutResourceId;
+		this.context = context;
+		this.data = data;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View row = convertView;
+		RecordHolder holder = null;
+
+		if (row == null) {
+			LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+			row = inflater.inflate(layoutResourceId, parent, false);
+
+			holder = new RecordHolder();
+			//Typeface font = Typeface.createFromAsset(context.getAssets(), "GenBasB.ttf");
+			holder.txtTitle = (TextView) row.findViewById(R.id.item_text);
+			//holder.txtTitle.setTypeface(font);
+			holder.txtString = (TextView) row.findViewById(R.id.item_text2);
+			//holder.txtString.setTypeface(font);
+			holder.imageItem = (ImageView) row.findViewById(R.id.item_image);
+			row.setTag(holder);
+		} else {
+			holder = (RecordHolder) row.getTag();
+		}
+
+		Item item = data.get(position);
+		holder.txtString.setText(item.getTitle());
+		holder.txtTitle.setText(item.getString());
+		holder.imageItem.setImageBitmap(item.getImage());
+		return row;
+
+	}
+	static class RecordHolder {
+		TextView txtTitle;
+		ImageView imageItem;
+		TextView txtString;
+
+	}
+}
