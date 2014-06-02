810cf34ba6857fbb0b19b2813fdbe794c2742141
Edwin Ko <eddersko@bu.edu>
2014-06-01 22:55:34 -0400

    Update AboutActivity.java

--- AboutActivity.java
+++ AboutActivity.java
@@ -19,7 +19,7 @@ public class AboutActivity extends Activity {
 	    image2.setImageResource(R.drawable.edna);
 	    String htmlText = "<html><body style=\"text-align:justify\"> %s </body></Html>";
         String myData = "This language app is part of a project to archive and make available materials on the Northern Pomo language, collected over 25 years. These material exist due to the dedication and work of Edna Campbell Guerrero and Elenor Stevenson Gonzales, both native speakers of Northern Pomo. This app was designed by Elodie Paquette, implemented in iOS by Elodie Paquette and Josh Leibsly, and implemented in Android by Edwin Ko. It was supported in part by a grant from the NEH/NSF Documenting Endangered Languages program (FN-50107-12) to Catherine O&apos;Connor, which is gratefully acknowledged. Views expressed in this material are the work of the authors, and do not necessarily reflect the views of the U.S. National Science Foundation or National Endowment for the Humanities. This material may not be sold.";
- 
+        setTitle("About This Application");
         WebView webView = (WebView) findViewById(R.id.webView1);
         WebSettings webSettings = webView.getSettings();
         webSettings.setDefaultFontSize(19);
